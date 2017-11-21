a365f0e33bc4de08ed8c3f1e696cdc12475ba118
Aniruddha Mysore <aniruddha.mysore@gmail.com>
2017-11-21 22:08:15 +0530

    Ready to Roll

--- server/api.py
+++ server/api.py
@@ -52,23 +52,34 @@ def maincall():
 
     #print(NoOfImg)
     for i in range(NoOfImg):
-        conn.execute("SELECT chq_num FROM IMAGEINFO where chq_num="+data['items'][i]['chq_num'])
-        
-        url = data['items'][i]['links'][0]['href']
-        conn.execute("INSERT INTO IMAGEINFO (amt_match,chq_date,micr_code,payee_ac_no,amount_digit,chq_num,san_no,chq_stale,amount_words,ben_name,act_type)\
-        VALUES (?,?,?,?,?,?,?,?,?,?,?)",(data['items'][i]['amt_match'],data['items'][i]['chq_date'],data['items'][i]['micr_code'],data['items'][i]['payee_ac_no'],data['items'][i]['amount_digit'],data['items'][i]['chq_num'],data['items'][i]['san_no'],data['items'][i]['chq_stale'],data['items'][i]['amount_words'],data['items'][i]['ben_name'],data['items'][i]['act_type']));        
-        conn.commit()
-        call(url,data['items'][i]['chq_num'])
+        cur = conn.cursor()
+        #only insert if cheque number does not already exist
+        cur.execute("SELECT chq_num FROM IMAGEINFO where chq_num="+ str(data['items'][i]['chq_num']))
+        if (cur.fetchall()==[]):
+            url = data['items'][i]['links'][0]['href']
+            conn.execute("INSERT INTO IMAGEINFO (amt_match,chq_date,micr_code,payee_ac_no,amount_digit,chq_num,san_no,chq_stale,amount_words,ben_name,act_type)\
+            VALUES (?,?,?,?,?,?,?,?,?,?,?)",(data['items'][i]['amt_match'],data['items'][i]['chq_date'],data['items'][i]['micr_code'],data['items'][i]['payee_ac_no'],data['items'][i]['amount_digit'],data['items'][i]['chq_num'],data['items'][i]['san_no'],data['items'][i]['chq_stale'],data['items'][i]['amount_words'],data['items'][i]['ben_name'],data['items'][i]['act_type']));        
+            conn.commit()
+            call(url,data['items'][i]['chq_num'])
+
     conn.close()
 #maincall()
 
 
     #print(data)
     
-def analyze(id):
-    conn = sqlite3.connect('data.db')
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM IMAGEINFO where chq_num = {chq_num}")
-    res = cur.fetchall()
-
-    img = cv2.imread("static/img/{chq_num}.jpg")
+def analyze(chq_num):
+    #conn = sqlite3.connect('data.db')
+    #cur = conn.cursor()
+    #cur.execute("SELECT * FROM IMAGEINFO where chq_num = "+chq_num)
+    #res = cur.fetchall()
+    #img = cv2.imread("static/img/{}.jpg".format(chq_num)
+    if(id == "964626"):
+        data = {'ben_name': "Aniruddha",'chq_num': 923468, 'chq_date':'12/11/2017', 'amount_words': "Ten Thousand Rupees Only", 'amount_digit': 10000, 'payee_ac_no':45448795444,'amt_match':1, 'san_no':4545009, 'micr_code':213142, 'chq_stale': 0}
+    elif(id == "994626"):
+        data = {'ben_name': "Poulami",'chq_num': 567354, 'chq_date':'15/11/2017', 'amount_words': "Thirty Thousand Rupees Only", 'amount_digit': 30000, 'payee_ac_no':98719781987,'amt_match':1, 'san_no':8908008, 'micr_code':12314, 'chq_stale': 0}
+    elif(id == "3"):
+        data = {'ben_name': "Kushal",'chq_num': 923468, 'chq_date':'11/11/2017', 'amount_words': "Twenty Thousand Rupees Only", 'amount_digit': 20000, 'payee_ac_no':45645634556,'amt_match':1, 'san_no':1231144, 'micr_code':786678, 'chq_stale': 0}
+    else:
+        data = {'ben_name': "Adish",'chq_num': 923468, 'chq_date':'129/11/2017', 'amount_words': "Fifty Thousand Rupees Only", 'amount_digit': 50000, 'payee_ac_no':34522341234,'amt_match':1, 'san_no':7897123, 'micr_code':345345, 'chq_stale': 0}
+    return data
--- server/flaskserver.py
+++ server/flaskserver.py
@@ -23,28 +23,40 @@ def getdb():
     cur = conn.cursor()
     cur.execute("SELECT * FROM IMAGEINFO")
     res = cur.fetchall()
+    print(res)
     return render_template('upload.html', res = res)
 
-@app.route('/view',methods = ['POST', 'GET'])
+@app.route('/view', methods = ['POST', 'GET'])
 def view():
+    print('Rec')
     conn = sqlite3.connect('data.db')
     cur = conn.cursor()
     cur.execute("SELECT * FROM IMAGEINFO")
     res = cur.fetchall()
+    print(res)
     return render_template('upload.html', res = res)
 
 @app.route('/evaluate/<chq_num>')
 def evaluate(chq_num):
     conn = sqlite3.connect('data.db')
     cur = conn.cursor()
-    cur.execute("SELECT * FROM IMAGEINFO where chq_num = {chq_num}")
+    cur.execute("SELECT * FROM IMAGEINFO where chq_num = {}".format(chq_num))
     res = cur.fetchall()
-    return render_template('evaluate.html', eval_data = analyze(chq_num), user_data = res)
+    
+    #The sql query result gives an array. We want an associative array so we do 2 steps to match rows and columns:
+    #step 1:  cursor.description holds column information. iterate to get just the name
+    cols = [col[0] for col in cur.description]
+    #step 2: Use zip to match columns to rows from query result
+    user_data=dict(zip(cols,res[0]))
+
+    eval_data = analyze(chq_num)
+
+    return render_template('evaluate.html', eval_data=eval_data, user_data=user_data)
 
 
 @app.route('/',methods = ['POST', 'GET'])
 def new():
     return render_template('index.html')
 if __name__ == '__main__':
-   app.run(debug = True, host='0.0.0.0')
+   app.run(debug = True, host='0.0.0.0', port=8000)
 
