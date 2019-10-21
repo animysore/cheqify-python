64e916af733d72d8ac1192ea767d70c761731e56
Adish Rao <adish.rao98@gmail.com>
2019-10-21 19:27:24 +0530

    Form filling creates new text file

--- server/static/js/init.js
+++ server/static/js/init.js
@@ -1,7 +1,3 @@
-(function($){
-  $(function(){
-
-    $('.button-collapse').sideNav();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+$(document).ready(function() {
+  $('.datepicker').datepicker();
+});
